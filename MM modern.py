a6a2e91c5273b49e44c0b7a3c45ba4f2cf36ef89
EEC <revenkov86@mail.ru>
2024-01-11 21:04:18 +0300

    +MM modern

--- MM modern.py
+++ MM modern.py
@@ -1,33 +1,20 @@
 import bs4
 from bs4 import BeautifulSoup
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.chrome.service import Service
+
 from webdriver_manager.chrome import ChromeDriverManager
 
+import pandas as pd
+
 baseURL = 'https://megamarket.ru'
-# target = input('target? ')
+# target = input('Ввести искомый товар')
 target = 'sun-m27bg130'
 targetURL = baseURL + '/catalog/?q=' + target.replace(' ', '%20')
-
-
-def get_source_html(url):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.maximize_window()
-
-    try:
-        driver.get(url=url)
-        WebDriverWait(driver, 60).until(ec.presence_of_element_located((By.TAG_NAME, "html")))
-        with open('source-page-mm.html', 'w', encoding='utf-8') as file:
-            file.write(driver.page_source)
-    except Exception as ex:
-        print(ex)
-    finally:
-        driver.close()
-        driver.quit()
-
 
 def get_items(file_path):
     with open(file_path, encoding='utf-8') as file:
@@ -36,7 +23,20 @@ def get_items(file_path):
     soup = BeautifulSoup(src, 'lxml')
     items_divs = soup.find_all('div', 'catalog-item-desktop')
 
-    items = {}
+    df = pd.DataFrame({'наименование': [], 'Полная цена': [], 'Кэшбек': [], 'Процент Кэшбека': [], 'Купон': [], 'Стоимость с плюшками': []})
+
+    def cupon(price):
+        if 11000 < price < 30000:
+            cupon1 = 2000
+        elif 30000 < price < 50000:
+            cupon1 = 5000
+        elif 50000 < price < 75000:
+            cupon1 = 9000
+        elif price > 75000:
+            cupon1 = 12000
+        else: cupon1 = 0
+        return cupon1
+
     for item in items_divs:
         item_block = item.find('div', class_='item-block')
         item_price_block = item_block.find('div', class_='inner catalog-item__prices-container')
@@ -50,27 +50,27 @@ def get_items(file_path):
             item_bonus_amount = item_bonus.find('span', class_='bonus-amount').get_text()
         else:
             continue
+        promo = 2000
+
 
         bonus = int(item_bonus_amount.replace(' ', ''))
+        bonus_percent = int(item_bonus_percent.replace('%', ''))
         price = int(item_price_result[0:-1].replace(' ', ''))
-        k = price / bonus
-        item_url = item.find('a')
+        # k = price / bonus
+        # item_url = item.find('a')
+        #
+        # link = baseURL + item_url.replace(' ', '%20')
+        # items[k] = {'price': item_price_result[0:-2], 'bonus amount': item_bonus_amount,
+        #             'bonus percent': item_bonus_percent, 'link': link}
+        best_price = (price - promo) * (1 - bonus_percent / 100)
+        df_item = pd.DataFrame([target, price, item_bonus_amount, item_bonus_percent, cupon(price), best_price])
+        df.append(df_item)
 
-        link = baseURL + item_url.replace(' ', '%20')
-        items[k] = {'price': item_price_result[0:-2], 'bonus amount': item_bonus_amount,
-                    'bonus percent': item_bonus_percent, 'link': link}
-
-    items = dict(sorted(items.items(), key=lambda x: x[0]))
-    for item in items:
-        print(f'{item} - {items[item]}')
-
-    return items
-
-
+    return df
 def main():
-    get_source_html(url=targetURL)
-    get_items(file_path='source-page-mm.html')
-
+    # get_source_html(url=targetURL)
+    # get_items(file_path='source-page-mm.html')
+    print(get_items(file_path='source-page-mm.html'))
 
 if __name__ == '__main__':
     main()
