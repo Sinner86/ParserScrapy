7e7d201fecf6b3f9ae618f55a13a485d1e2a95c8
EEC <revenkov86@mail.ru>
2024-01-16 17:57:35 +0300

    MM parser v 1.0

--- MM modern.py
+++ MM modern.py
@@ -2,34 +2,44 @@ import parser_function as pf
 import pandas as pd
 # import openpyxl
 
-
+# список наименований товаров для поиска
+# targets = ['rtx 3060', 'rtx 4060', 'Xiaomi X27G', 'sun-m27bg130']
 targets = ['rtx 3060', 'rtx 4060']
-# targets = ['rtx 3060']
+links = ['https://megamarket.ru/catalog/details/monitor-xiaomi-x27g-g27-165hz-1920x1080-ips-600013078509/#?details_block=prices']
+
 def main(targets):
     baseURL = 'https://megamarket.ru'
     targets = targets
     path = 'output.xlsx'
 
-    # return pf.get_items(file_path='source-page-mm.html')
-    # with open(path, mode="a+") as writer:
-
-    # writer = open(path, 'a+')
-    # try:
-    #     for target in targets:
-    #         targetURL = baseURL + '/catalog/?q=' + target.replace(' ', '%20')
-    #         pf.get_source_html(url=targetURL)
-    #         info = pf.get_items(file_path='source-page-mm.html')
-    #         info.to_excel(path, sheet_name=f'{target}')
-    # finally:
-    #     writer.close()
-
+    # подготовка файла для экспорта данных
     writer = pd.ExcelWriter(path)
-    for target in targets:
-        targetURL = baseURL + '/catalog/?q=' + target.replace(' ', '%20')
-        pf.get_source_html(url=targetURL)
-        info = pf.get_items(file_path='source-page-mm.html')
-        info.to_excel(writer, sheet_name=f'{target}')
-    writer.close()
+    try:
+        for target in targets:
+            # подготовка ссылки
+            targetURL = baseURL + '/catalog/?q=' + target.replace(' ', '%20')
+            # запуск драйвера браузера и получение данных
+            pf.get_source_html(url=targetURL)
+            # переработка полученных данных в таблицу
+            info = pf.get_items(file_path='source-page-mm.html')
+            # запись в файл
+            info.to_excel(writer, sheet_name=f'{target}')
+    finally:
+        writer.close()
 
+def main2(link):
+    path = 'output_page.xlsx'
+    writer = pd.ExcelWriter(path)
+    try:
+        for link in links:
+            # запуск драйвера браузера и получение данных
+            pf.get_source_html(url=link)
+            # переработка полученных данных в таблицу
+            info = pf.get_items(file_path='source-page-mm.html')
+            # запись в файл
+            info.to_excel(writer)
+    finally:
+        writer.close()
 if __name__ == '__main__':
-    main(targets)
+    # main(targets)
+    main2(links)
--- parser_function.py
+++ parser_function.py
@@ -83,3 +83,63 @@ def get_items(file_path):
         df = df._append(df_item)
 
     return df.sort_values(by=['Стоимость с плюшками'])
+
+# функция сбора информации с определенной страницы ММ
+def get_items_page(file_path):
+    with open(file_path, encoding='utf-8') as file:
+        src = file.read()
+    soup = BeautifulSoup(src, 'lxml')
+    # поиск блоков
+    items_divs = soup.find_all('div', 'product-offer product-offer_with-payment-method')
+    # подготовка пустого датафреймас именованными колонками
+    df = pd.DataFrame({'наименование': [], 'Магазин': [], 'Полная цена': [], 'Кэшбек': [], 'Процент Кэшбека': [], 'Купон': [],
+                       'Стоимость с плюшками': [], 'Ссылка': []})
+
+    # функция определения купона по стоимости товара
+    def cupon(price):
+        if 11000 < price < 30000:
+            cupon_finish = 2000
+        elif 30000 < price < 50000:
+            cupon_finish = 5000
+        elif 50000 < price < 75000:
+            cupon_finish = 9000
+        elif price > 75000:
+            cupon_finish = 12000
+        else:
+            cupon_finish = 0
+        return cupon_finish
+
+    # перебор блоков с товарами
+    for item in items_divs:
+        product_offer_price = item.find('div', class_='product-offer-price')
+
+        # получение полной цены
+        price = product_offer_price.find('span').get_text()
+        price = int(price.replace(' ', ''))
+
+        # получение наименования товар
+        header = soup.find('h1', class_='pdp-header__title pdp-header__title_only-title')
+        name = header.get_text()
+
+        # получение бонусов
+        item_bonus_block = product_offer_price.find('div', class_='product-offer-price-bonus product-offer-price-bonus_with-money-bonus')
+        item_bonus = item_bonus_block.find('div', class_='money-bonus xs money-bonus_loyalty')
+        if isinstance(item_bonus, bs4.element.Tag):
+            bonus_percent = int(item_bonus.find('span', class_='bonus-percent').get_text().replace('%', ''))
+            bonus_amount = int(item_bonus.find('span', class_='bonus-amount').get_text().replace(' ', ''))
+        else:
+            continue
+        # получение названия магазина
+        product_offer_name = item.find('div', class_='product-offer-name')
+        market_name = product_offer_name.find('span', class_='pdp-merchant-rating-block__merchant-name').get_text()
+
+        # расчет цены с учетом купона и кэшбека
+        best_price = (price - cupon(price)) * (1 - bonus_percent / 100)
+
+        # составление строки с данными и добавление к датафрейму
+        df_item = pd.DataFrame({'наименование': [name], 'Магазин': [market_name], 'Полная цена': [price], 'Кэшбек': [bonus_amount],
+                                'Процент Кэшбека': [bonus_percent], 'Купон': [cupon(price)],
+                                'Стоимость с плюшками': [best_price], 'Ссылка': [link]})
+        df = df._append(df_item)
+
+    return df.sort_values(by=['Стоимость с плюшками'])
